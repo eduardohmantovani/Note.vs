6dc43d2ca3bac53afaf66722539cae1dc9516924
Eduardo Mantovani <eduardohmantovani@outlook.com>
2021-05-08 16:48:10 -0300

    visor

--- Calculadora.js
+++ Calculadora.js
@@ -1,6 +1,7 @@
 var operacao = '';
-var auxInputClick = 'numero1';
-var auxInputClick2 ='numero2';
+var isInputNumero1 = true;
+var numero1 = '';
+var numero2 = '';
 
 function somarDoisNumeros(numero1, numero2) {
     return numero1 + numero2;
@@ -24,8 +25,8 @@ function dividirDoisNumeros(numero1, numero2) {
 
 function calcular() {
     let numero1, numero2;
-    numero1 = Number(document.getElementById('numero1').value);
-    numero2 = Number(document.getElementById('numero2').value);
+    numero1 = Number(this.numero1);
+    numero2 = Number(this.numero2);
 
     if (validarSeAsEntradasSaoVazias(numero1, numero2)) {
         let resultado;
@@ -39,37 +40,28 @@ function calcular() {
             resultado = dividirDoisNumeros(numero1, numero2);
         }
 
-        document.getElementById('resultado').value = resultado;
-        estadoInicial();
+        setInputResultado(resultado);
+        estadoInicialPropriedades();
     } else {
         alert('Digite os dois números');
     }
 }
 
-function validarNumero(inputText) {
-    let valorConvertido = Number(inputText.value);
-    if (isNaN(valorConvertido)) {
-        alert('Digite um número válido.');
-        inputText.value = "";
-        inputText.focus();
-    }
+function setInputResultado(valor) {
+    document.getElementById('resultado').value = valor;
+    document.getElementById('numero1').value = numero1;
+    document.getElementById('numero2').value = numero2;
 }
 
 function validarSeAsEntradasSaoVazias(numero1, numero2) {
     return !((numero1 == '') || (numero2 == ''));
 }
 
-function limparCampos() {
-    document.getElementById('numero1').value = "";
-    document.getElementById('numero2').value = "";
-}
-
-function estadoInicial() {
-    limparCampos();
-    document.getElementById('numero1').focus();
+function estadoInicialPropriedades() {
     this.operacao = '';
-    this.auxInputClick = 'numero1';
-    this.auxInputClick2 = 'numero2';
+    this.isInputNumero1 = true;
+    this.numero1 = '';
+    this.numero2 = '';
 }
 
 function clickNumero(valor) {
@@ -77,17 +69,34 @@ function clickNumero(valor) {
 }
 
 function clickDecimal() {
-    if (document.getElementById(this.auxInputClick).value.indexOf('.') < 0) {
+    let valorValidar = '';
+    if (this.isInputNumero1) {
+        valorValidar = this.numero1;
+    } else {
+        valorValidar = this.numero2;
+    }
+
+    if (valorValidar.indexOf('.') < 0) {
         setValor(".");
     }
 }
 
 function setValor(valor) {
-    let valorAtual = document.getElementById(this.auxInputClick).value;
-    document.getElementById(this.auxInputClick).value = valorAtual + valor;
+    if (this.isInputNumero1) {
+        this.numero1 = this.numero1 + valor;
+        setInputResultado(this.numero1);
+    } else {
+        this.numero2 = this.numero2 + valor;
+        setInputResultado(this.numero2);
+    }
 }
 
 function clickOperacao(paramOperacao) {
     this.operacao = paramOperacao;
-    this.auxInputClick = 'numero2';
+    this.isInputNumero1 = false;
+}
+
+function limparCalculadora() {
+    estadoInicialPropriedades();
+    setInputResultado('');
 }
